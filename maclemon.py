a8b7bcc371a4db308145fc7d7c02ca03c8a9d34a
Matte <mattias4@kth.se>
2018-12-02 22:01:33 +0100

    Revert "Ran 2to3 on maclemon.py"
    This reverts commit 08e96f911ed14c464e9cc7678c37cbee5a27f4e5.

--- maclemon.py
+++ maclemon.py
@@ -1,57 +1,57 @@
 #!/usr/bin/python 
 
-from io import StringIO
-import time, subprocess, os
+from cStringIO import StringIO
+import time, commands, os
 from sys import argv
 
 def method1():
 	out_str = ''
-	for num in range(loop_count):
-		out_str += repr(num)
+	for num in xrange(loop_count):
+		out_str += `num`
 	ps_stats()
 	return out_str
 
 def method2():
-	from collections import MutableString
+	from UserString import MutableString
 	out_str = MutableString()
-	for num in range(loop_count):
-		out_str += repr(num)
+	for num in xrange(loop_count):
+		out_str += `num`
 	ps_stats()
 	return out_str
 
 def method3():
 	from array import array
 	char_array = array('c')
-	for num in range(loop_count):
-		char_array.fromstring(repr(num))
+	for num in xrange(loop_count):
+		char_array.fromstring(`num`)
 	ps_stats()
 	return char_array.tostring()
 
 def method4():
 	str_list = []
-	for num in range(loop_count):
-		str_list.append(repr(num))
+	for num in xrange(loop_count):
+		str_list.append(`num`)
 	out_str = ''.join(str_list)
 	ps_stats()
 	return out_str
 
 def method5():
 	file_str = StringIO()
-	for num in range(loop_count):
-		file_str.write(repr(num))
+	for num in xrange(loop_count):
+		file_str.write(`num`)
 	out_str = file_str.getvalue()
 	ps_stats()
 	return out_str
 
 def method6():
-	out_str = ''.join([repr(num) for num in range(loop_count)])
+	out_str = ''.join([`num` for num in xrange(loop_count)])
 	ps_stats()
 	return out_str
 
 
 def ps_stats():
 	global process_size
-	ps = subprocess.getoutput('ps -up ' + repr(pid))
+	ps = commands.getoutput('ps -up ' + `pid`)
 	process_size = ps.split()[15]
 
 def call_method(num):
@@ -59,11 +59,11 @@ def call_method(num):
 	start = time.time()
 	z = eval('method' + str(num))()
 	end = time.time()
-	print("method", num)
-	print("time", float((end-start) * 1000), "ms")
-	print("output size ", len(z) / 1024, "kb")
-	print("process size", process_size, "kb")
-	print()
+	print "method", num
+	print "time", float((end-start) * 1000), "ms"
+	print "output size ", len(z) / 1024, "kb"
+	print "process size", process_size, "kb"
+	print
 	
 loop_count = 20000
 pid = os.getpid()
@@ -71,5 +71,5 @@ pid = os.getpid()
 if len(argv) == 2:
 	call_method(argv[1])
 else:
-	print("Usage: python stest.py <n>\n" \
-		"  where n is the method number to test")
+	print "Usage: python stest.py <n>\n" \
+		"  where n is the method number to test"
