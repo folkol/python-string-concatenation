2eb899ca85f8c6eaf13c68621d513ed5701cad62
Matte <mattias.johansson@ivbar.com>
2018-12-04 00:23:33 +0100

    Plotted some histograms of runtimes with r1 and r2

--- hist.py
+++ hist.py
@@ -3,22 +3,26 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.loadtxt(sys.stdin)
-
-print(x.max())
-print(x.min())
-# plt.style.use('ggplot')
-# plt.style.use('bmh')
-
+colors = 'rgbmcb'
 # for style in plt.style.available:
-# plt.style.use(style)
-# plt.style.use('ggplot')
-# plt.yscale('log')
-n, bins, patches = plt.hist(x, 100, density=True, facecolor='g', alpha=0.75)
-n, bins, patches = plt.hist(x * 2, 100, density=True, facecolor='b', alpha=0.75)
+style = 'ggplot'
+for c, file in enumerate(sys.argv[1:]):
+    x = np.loadtxt(file)
 
-plt.xlabel('Latency')
-plt.ylabel('#')
-plt.title('Elapsed time')
-plt.grid(True)
+    # plt.style.use('ggplot')
+    # plt.style.use('bmh')
+
+    # for style in plt.style.available:
+    # plt.style.use(style)
+    plt.style.use(style)
+    plt.yscale('log')
+    # n, bins, patches = plt.hist(x, 30, facecolor=colors[c], edgecolor='black', density=False, alpha=0.3, label=file)
+    n, bins, patches = plt.hist(x, 100, edgecolor='black', density=False, alpha=.5, label=file)
+    # n, bins, patches = plt.hist(x * 2, 100, density=True, facecolor='b', alpha=0.75)
+
+    plt.xlabel('Latency')
+    plt.ylabel('#')
+    plt.title(f'Elapsed time {style}')
+    plt.grid(True)
+plt.legend()
 plt.show()
---
+++ mclemon_r2.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python 
+
+from cStringIO import StringIO
+import time, commands, os
+from sys import argv
+
+def method1():
+	out_str = ''
+	for num in xrange(loop_count):
+		out_str += `num`
+	return out_str
+
+def method2():
+	from UserString import MutableString
+	out_str = MutableString()
+	for num in xrange(loop_count):
+		out_str += `num`
+	return out_str
+
+def method3():
+	from array import array
+	char_array = array('c')
+	for num in xrange(loop_count):
+		char_array.fromstring(`num`)
+	return char_array.tostring()
+
+def method4():
+	str_list = []
+	for num in xrange(loop_count):
+		str_list.append(`num`)
+	out_str = ''.join(str_list)
+	return out_str
+
+def method5():
+	file_str = StringIO()
+	for num in xrange(loop_count):
+		file_str.write(`num`)
+	out_str = file_str.getvalue()
+	return out_str
+
+def method6():
+	out_str = ''.join([`num` for num in xrange(loop_count)])
+	return out_str
+
+
+def ps_stats():
+	global process_size
+	process_size = commands.getoutput('ps -o vsz= -p ' + `pid`)
+	# process_size = ps.split()[15]
+
+def call_method(num):
+	global process_size
+	start = time.time()
+	z = eval('method' + str(num))()
+	end = time.time()
+	ps_stats()
+	print "method", num
+	print "time", float((end-start) * 1000), "ms"
+	print "output size ", len(z) / 1024, "kb"
+	print "process size", process_size, "kb"
+	print
+	
+loop_count = 20000
+pid = os.getpid()
+
+if len(argv) == 2:
+	call_method(argv[1])
+else:
+	print "Usage: python stest.py <n>\n" \
+		"  where n is the method number to test"
